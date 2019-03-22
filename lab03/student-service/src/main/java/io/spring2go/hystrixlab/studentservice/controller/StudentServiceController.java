ebc3fdeba887c7d146b1b576eccaa249c598f8a8
王青松 <wang.qingsong@navercorp.com>
2019-03-22 14:55:40 +0800

    Test

--- lab03/student-service/src/main/java/io/spring2go/hystrixlab/studentservice/controller/StudentServiceController.java
+++ lab03/student-service/src/main/java/io/spring2go/hystrixlab/studentservice/controller/StudentServiceController.java
@@ -15,59 +15,56 @@ import io.spring2go.hystrixlab.studentservice.domain.Student;
 
 @RestController
 public class StudentServiceController {
-	
+
 	private static Map<String, List<Student>> schoolDB = new HashMap<String, List<Student>>();
-	
+
 	static {
 		schoolDB = new HashMap<String, List<Student>>();
-		
+
 		List<Student> students = new ArrayList<Student>();
-		Student student = new Student("BoBo", "Class I");
+		Student student = new Student("Rose", "Class I");
 		students.add(student);
 		student = new Student("Jack", "Class II");
 		students.add(student);
-		
+
 		schoolDB.put("abcschool", students);
-		
+
 		students = new ArrayList<Student>();
 		student = new Student("Daniel", "Class III");
 		students.add(student);
 		student = new Student("William", "Class V");
 		students.add(student);
-		
+
 		schoolDB.put("xyzschool", students);
 	}
-	
+
 	@RequestMapping(value = "/getStudentBySchool/{schoolName}", method = RequestMethod.GET)
 	public List<Student> getStudents(@PathVariable String schoolName) {
 		System.out.println("Getting student for " + schoolName);
-		
-		randomlyRunLong();
-		
+
+		randomlySleep();
+
 		List<Student> studentList = schoolDB.get(schoolName);
 		if (studentList == null) {
 			studentList = new ArrayList<Student>();
 			Student student = new Student("Not Found", "N/A");
 			studentList.add(student);
 		}
-		
+
 		return studentList;
 	}
-	
-	private void randomlyRunLong() {
+
+	private void randomlySleep() {
 		Random rand = new Random();
-		
+
 		int randomNum = rand.nextInt(3) + 1;
-		
-		if (randomNum == 3) sleep();
-	}
-	
-	private void sleep() {
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+
+		if (randomNum == 3) {
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 	}
-
 }
